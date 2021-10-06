2f8749fd4f7b07c3134495ef39228ffdf9dc79fb
Linda Denissen <lintje82@gmail.com>
2021-10-06 09:04:49 +0200

    array methods deel 1

--- array methods deel 1/script.js
+++ array methods deel 1/script.js
@@ -1,1 +1,7 @@
-// Oefening 1
+const addTheWordCool = function(array){
+  // add your code
+let array = ["nice", "awesome", "tof"]
+
+};
+console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+// resultaat: ["nice", "awesome", "tof", "cool"]
